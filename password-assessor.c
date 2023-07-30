fc0a7def2b827b4a828d96c096235db7368fb503
g0nchy <134018418+g0nchy@users.noreply.github.com>
2023-07-30 03:15:45 -0300

    Update password-assessor.c
    Added a verbose mode

--- password-assessor.c
+++ password-assessor.c
@@ -14,24 +14,32 @@ enum PasswordStrength {
 
 // Constants for password evaluation
 const int MIN_PASSWORD_LENGTH = 0;
-const int MAX_PASSWORD_LENGTH = 18;
 const int WEAK_PASSWORD_THRESHOLD = 5;
 const int POOR_PASSWORD_THRESHOLD = 10;
 const int GOOD_PASSWORD_THRESHOLD = 13;
 const int EXCELLENT_PASSWORD_THRESHOLD = 16;
+const int MAX_PASSWORD_LENGTH = 18;
 
 // Function declarations
-int count_uppercase_letters(const string password);
-int count_lowercase_letters(const string password);
-int count_numbers(const string password);
-int count_special(const string password);
-enum PasswordStrength evaluate_password_strength(const string password);
+int count_uppercase_letters(const string password, int password_length);
+int count_lowercase_letters(const string password, int password_length);
+int count_numbers(const string password, int password_length);
+int count_special(const string password, int password_length);
+enum PasswordStrength evaluate_password_strength(int UL, int LL, int N, int S);
+void display_verbose_information(int UL, int LL, int N, int S);
 
-int main(void)
+int main(int argc, string argv[])
 {
     // Variables
     string password;
     int password_length;
+
+    // Check for verbose flag
+    bool verbose = false;
+    if (argc == 2 && (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--verbose") == 0))
+    {
+        verbose = true;
+    }
 
     // Get user input for password and validate its length
     do
@@ -46,8 +54,14 @@ int main(void)
     }
     while (password_length <= MIN_PASSWORD_LENGTH || password_length > MAX_PASSWORD_LENGTH);
 
-    // Evaluate password strength
-    enum PasswordStrength strength = evaluate_password_strength(password);
+    // Count specific characters in the password
+    int UL = count_uppercase_letters(password, password_length);
+    int LL = count_lowercase_letters(password, password_length);
+    int N = count_numbers(password, password_length);
+    int S = count_special(password, password_length);
+
+    // Evaluate password and display results
+    enum PasswordStrength strength = evaluate_password_strength(UL, LL, N, S);
 
     // Display password strength result
     switch (strength)
@@ -73,14 +87,20 @@ int main(void)
             break;
     }
 
+    // Display verbose information if the verbose flag is present
+    if (verbose)
+    {
+        display_verbose_information(UL, LL, N, S);
+    }
+
     return 0;
 }
 
 // Functions to count specific characters in the password
-int count_uppercase_letters(const string password)
+int count_uppercase_letters(const string password, int password_length)
 {
     int uppercase_letters = 0;
-    for (int i = 0, len = strlen(password); i < len; i++)
+    for (int i = 0; i < password_length; i++)
     {
         if (isupper(password[i]))
         {
@@ -90,10 +110,10 @@ int count_uppercase_letters(const string password)
     return uppercase_letters;
 }
 
-int count_lowercase_letters(const string password)
+int count_lowercase_letters(const string password, int password_length)
 {
     int lowercase_letters = 0;
-    for (int i = 0, len = strlen(password); i < len; i++)
+    for (int i = 0; i < password_length; i++)
     {
         if (islower(password[i]))
         {
@@ -103,10 +123,10 @@ int count_lowercase_letters(const string password)
     return lowercase_letters;
 }
 
-int count_numbers(const string password)
+int count_numbers(const string password, int password_length)
 {
     int numbers = 0;
-    for (int i = 0, len = strlen(password); i < len; i++)
+    for (int i = 0; i < password_length; i++)
     {
         if (isdigit(password[i]))
         {
@@ -116,10 +136,10 @@ int count_numbers(const string password)
     return numbers;
 }
 
-int count_special(const string password)
+int count_special(const string password, int password_length)
 {
     int special = 0;
-    for (int i = 0, len = strlen(password); i < len; i++)
+    for (int i = 0; i < password_length; i++)
     {
         if (ispunct(password[i]))
         {
@@ -130,14 +150,9 @@ int count_special(const string password)
 }
 
 // Function to evaluate password strength based on character counts
-enum PasswordStrength evaluate_password_strength(const string password)
+enum PasswordStrength evaluate_password_strength(int UL, int LL, int N, int S)
 {
-    int password_length = strlen(password);
-    int UL = count_uppercase_letters(password);
-    int LL = count_lowercase_letters(password);
     int L = UL + LL;
-    int N = count_numbers(password);
-    int S = count_special(password);
     int T = L + N + S;
 
     if ((T == N && T <= POOR_PASSWORD_THRESHOLD) || (T == LL && T <= 7) || (LL > 0 && UL > 0 && L == T && T <= 6) || (LL > 0 && UL > 0 && N > 0 && L + N == T && T <= WEAK_PASSWORD_THRESHOLD) || (LL > 0 && UL > 0 && N > 0 && S > 0 && T <= WEAK_PASSWORD_THRESHOLD))
@@ -160,4 +175,14 @@ enum PasswordStrength evaluate_password_strength(const string password)
     {
         return UNKNOWN;
     }
+}
+
+// Function to evaluate and display verbose information about the password
+void display_verbose_information(int UL, int LL, int N, int S)
+{
+    printf("\nVerbose Information:\n");
+    printf("Uppercase letters: %d\n", UL);
+    printf("Lowercase letters: %d\n", LL);
+    printf("Numbers: %d\n", N);
+    printf("Special characters: %d\n", S);
 }
